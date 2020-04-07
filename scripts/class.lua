02be83a7ea3f08c8ec581fb30010f2afd3b4964f
danilacasito <54474838+danilacasito@users.noreply.github.com>
2020-04-07 20:16:30 +0200

    Update class.lua

--- scripts/class.lua
+++ scripts/class.lua
@@ -1,3 +1,6 @@
+version = 1.0
+desktop = "/scripts/daniel"
+print("Los Desarrolladores estan desarrollando una nueva Version de Class os (Version 2.0) Y Dentro de Poco se Podra Descargar")
 -- Boot
 boot = paintutils.loadImage("/scripts/bootlogo.nfp")
 paintutils.drawImage(boot, 1, 1)
