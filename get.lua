f6342392f09bda1b66ac0757e736cc9f49bb82ee
danilacasito <54474838+danilacasito@users.noreply.github.com>
2020-04-07 17:35:05 +0200

    NUEVO: Muestra la Lista de Paquetes
    Muestra la lista de los paquetes?

--- get.lua
+++ get.lua
@@ -7,6 +7,10 @@ if targ == 0 then
 end
 if targ[1] == "install" then
     shell.run("wget https://raw.githubusercontent.com/danilacasito/cprogram/master/"..targ[2]..".lua")
+elseif targ[1] == "list" then
+    shell.run("wget https://raw.githubusercontent.com/danilacasito/cprogram/master/programs.list")
+    shell.run("programs.list")
+    shell.run("rm programs.list")
 elseif targ[1] == "repo-edit" then
     shell.run("edit /getlist.lua")
 end
