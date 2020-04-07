f73733ca951cf9a061972cb4169666350153a137
danilacasito <54474838+danilacasito@users.noreply.github.com>
2020-04-07 18:40:40 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -7,5 +7,16 @@ print("Ok, You have 128 kB of RAM")
 sleep(1)
 write("Detecting OS... ")
 sleep(1)
-print("OK, Starting Class")
-shell.run("/scripts/class.lua")
+shell.run("clear")
+print("CLASS BOOT LOADER")
+print("class_os")
+print("craft_os")
+sel = read()
+if sel == "class_os" then
+   shell.run("/scripts/class.lua")
+elseif sel == "craft_os" then
+   shell.run("shell")
+else
+   print("option")
+   os.reboot()
+end
